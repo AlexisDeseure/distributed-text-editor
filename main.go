be186db9e9d0356b9bf568aed8cc1cdd3a84110d
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2025-05-07 11:09:10 +0200

    clean comment

--- main.go
+++ main.go
@@ -99,43 +99,9 @@ func mergeClocks(local VectorClock, remote VectorClock) {
 	}
 }
 
-// File where the text will be saved
-// const saveFilePath = "save.txt"
-
 // Interval in seconds between autosaves
 const autoSaveInterval = 1 * time.Second
 
-// Write the text into the save file
-// func saveTextToFile(path string, content string) error {
-
-// 	return os.WriteFile(path, []byte(content), 0644)
-// }
-
-// Process the current text, and apply some operations
-// The minimum is to save the text into the file
-// Then the text can also be sent to another connected device
-// This method is called periodically (see 'autoSaveInterval'), and one last time when the app closes
-// It is never called if the text has not changed since last save
-// func processText(oldContent string, newContent string) error {
-
-// 	// Save the text and get a possible error
-// 	err := saveTextToFile(saveFilePath, newContent)
-
-// 	difftools.SaveModifs(oldContent, newContent)
-
-// 	// Print the error
-// 	if err != nil {
-
-// 		fmt.Println("Error while saving:", err)
-
-// 	// Notify that the text was successfully saved
-// 	} else {
-
-// 		fmt.Println("Autosave...")
-// 	}
-
-// 	return err
-// }
 
 func main() {
 
@@ -172,64 +138,6 @@ func main() {
 	// Define the visible size of the scrollable area
 	scrollable.SetMinSize(fyne.NewSize(600, 400))
 
-	/*// Create a channel to communicate with the save goroutine
-	// When the app closes, a boolean is passed to this channel to tell the goroutine to save one last time and stop
-	stopAutoSave := make(chan bool)
-
-	// Launch a goroutine to manage autosaving
-	go func() {
-
-		// Create a ticker (clock) so the goroutine knows when to perform a save
-		ticker := time.NewTicker(autoSaveInterval)
-
-		// Tell the goroutine to stop the clock when the app closes
-		defer ticker.Stop()
-
-		// Save the last saved version of the text, so that the goroutine knows when the text has changed
-		lastSaved := textArea.Text
-
-		// Infinite loop (ends when the goroutine returns)
-		for {
-
-			// Two processed cases
-			select {
-
-			// The interval time is up
-			case <-ticker.C:
-
-				// Get the current text from the text area
-				current := textArea.Text
-
-				// No need to save if the text has not changed
-				if current != lastSaved {
-
-					// Process the text
-					// This includes saving it into a file, but eventually other actions could be performed
-					processText(lastSaved, current)
-
-					// Update the last saved version
-					lastSaved = current
-				}
-
-			// The app is closing, a signal was sent to this goroutine
-			case <-stopAutoSave:
-
-				// Get the current text from the text area
-				current := textArea.Text
-
-				// No need to save if the text has not changed
-				if current != lastSaved {
-
-					// Process the text
-					// This includes saving it into a file, but eventually other actions could be performed
-					processText(lastSaved, current)
-				}
-
-				// Stop the goroutine
-				return
-			}
-		}
-	}()*/
 
 	// Goroutine de réception et application des horloges
 	go func() {
